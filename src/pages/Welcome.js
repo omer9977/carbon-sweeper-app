3a0dd05be7042742ec3bc0db437b312e7d11db69
Cem Kağan Kayılı <cemkagankayili@hotmail.com>
2023-05-06 15:41:50 +0300

    Welcoma page initials

---
+++ src/pages/Welcome.js
@@ -0,0 +1,13 @@
+import "../css/welcome.css"
+
+function Welcome() {
+    return (
+        <div>
+            <nav>
+                <h1>Carbon Sweeper</h1>
+            </nav>
+        </div>
+    );
+}
+
+export default Welcome;
