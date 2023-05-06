9c8381f36feeb9413c20ff2a5e791323c9557ce7
Cem Kağan Kayılı <cemkagankayili@hotmail.com>
2023-05-06 18:44:17 +0300

    Welcomepage only buttons left

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,15 +1,23 @@
+import "../css/stats.css" 
+
 function Stats() {
     const seed = Math.floor(Math.random() * 1000);
-    const totalUsers = <div>{seed}</div> 
-    const savedTrees = <div>{Math.floor(seed * 0.8)}</div>
-    const emissions = 0.1;
+    const totalUsers = <div className="number">{seed}</div> 
+    const savedTrees = <div className="number">{Math.floor(seed * 0.8)}</div>
+    const emissions = <div className="number">0.1</div>
 
 
     return (
-        <div>
-            <h2>Total users: {totalUsers}</h2>
-            <h2>Saved trees: {savedTrees}</h2>
-            <h2>Total emissions: {emissions}</h2>
+        <div className="content">
+            <div>
+                <img src="https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="house" />
+            </div>
+            <div>
+                <div className="statsec">Total users: {totalUsers}</div>
+                <div className="statsec">Saved trees: {savedTrees}</div>
+                <div className="statsec">Total emissions: {emissions}</div>
+            </div>
+            
         </div>
     );
 }
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -11,7 +11,6 @@ function Welcome() {
             </nav>
             <h2>Welcome to Carbon Sweeper!</h2>
             <div className="stats">
-                <img src="https://www.energysage.com/wp-content/uploads/2020/05/energy-efficient-home.jpg" alt="house" />
                 <Stats />
                 
             </div>
